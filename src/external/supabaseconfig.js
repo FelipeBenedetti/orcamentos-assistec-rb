397b0b970771452fb051dd4fe88f93afcfc63b27
Felipe Benedetti <felipe-benedetti@hotmail.com>
2025-02-10 21:36:01 -0300

    remove logs

--- src/external/supabaseconfig.js
+++ src/external/supabaseconfig.js
@@ -1,13 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-////const supabaseURL = process.env.REACT_APP_SUPABASE_URL;
-//const supabaseURL = 'https://fotkuvjbapiisacckqei.supabase.co';
-////const supabaseKEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
-//const supabaseKEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvdGt1dmpiYXBpaXNhY2NrcWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwODE2NzYsImV4cCI6MjA1MzY1NzY3Nn0.yCMXgsK86ndLTbYIdvjN5WbVBTtFcd5bMSwDevdz2y0';
-//console.log('URL: ', supabaseURL, 'Key: ', supabaseKEY);
-//const supabase = createClient(supabaseURL, supabaseKEY);
-//export default supabase;
-
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY,
