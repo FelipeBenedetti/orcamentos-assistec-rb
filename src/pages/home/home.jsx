ec7e01d9488d0ceffd892b7488b31974f273b221
Felipe Benedetti <felipe-benedetti@hotmail.com>
2025-02-10 22:04:10 -0300

    add game home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,25 @@
-
 function Home() {
   return (
-    <div>
-      <h1>Barbearia do Eliseu</h1>
-    </div>
+    <>
+      <h1 style={{display:"flex", justifyContent: "center"}}>Barbearia do Eliseu</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center", // Centraliza horizontalmente
+          height: "100vh",          // Ocupa 100% da altura da viewport (para centralização vertical)
+        }}
+      >
+        <iframe
+          src="https://cdn.htmlgames.com/HappyBird/"
+          width="1000"
+          height="600"
+          allowFullScreen
+          title="Jogo Embutido"
+        ></iframe>
+      </div>
+    </>
   );
 }
 
 export default Home;
+
